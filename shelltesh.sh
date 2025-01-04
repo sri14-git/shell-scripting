be5992df3c6eef003fe284eaab485e9ecd38bdd0
sri14 <srivarshan14@gmail.com>
2025-01-04 06:01:01 +0000

    revised shell and arrays

--- shelltesh.sh
+++ shelltesh.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
-
+name="hello"
+name2="world"
 read -p "enter a number" n1
 read -p "enter another number" n2
-
+echo "$name $name2"
 echo "enter the action M, A or S"
 read ch
 case $ch in
@@ -16,4 +17,10 @@ case $ch in
         echo "$(($n1-$n2))" | tee shellwrite.txt 
         ;;
 esac
-
+if [ $ch == M ] ; then #{$1,,}
+    echo "the choice is M"
+fi
+array_name=(1 2 3 4 5 6)
+for item in "${array_name[@]}"; do
+    echo $item
+done
